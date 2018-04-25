d4221ce06fdde334a35a65985d1c1ee6f76f339b
Jim Holmes <frazzleddad@gmail.com>
2018-04-24 17:33:27 -0700

    Initial project with libs and one starting file.

---
+++ SaneWebDriver-Java/src/FirstTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+class FirstTest {
+
+	@Test
+	void check_grid_loads() {
+		//create a Firefox thing
+		//start Firefox
+		//go to the page
+		//see if the thing we want to check is on page
+		//stop Firefox
+	}
+
+}
