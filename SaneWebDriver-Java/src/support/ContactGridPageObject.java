6341557fba81938d0ce11992e17da272f530fc3c
Jim Holmes <frazzleddad@gmail.com>
2018-05-12 11:01:13 +0200

    Cleanup of class casing, naming, etc.

--- SaneWebDriver-Java/src/support/ContactGridPageObject.java
+++ SaneWebDriver-Java/src/support/ContactGridPageObject.java
@@ -7,28 +7,43 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-/* TestInstance(Lifecycle.PER_CLASS) enables use of
- *   @BeforeClass on non-static methods.
- *   See discussion on how JUnit handles test class lifecycle at
- *   https://stackoverflow.com/questions/1052577/why-must-junits-fixturesetup-be-static 
- */
 
     public class ContactGridPageObject
     {
-        public static String GRID_ID = "grid";
+    	public static String GRID_ID = "grid";
         public static String CREATE_BTN_ID = "create_btn";
 
-        public WebElement GetContactGrid(WebDriver browser)
+        public WebElement CreateButton;
+
+        private WebDriver browser;
+
+        public ContactGridPageObject (WebDriver browser)
+        {
+            this.browser = browser;
+            WebDriverWait wait = new WebDriverWait(browser,30);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.id(CREATE_BTN_ID)));
+            CreateButton = browser.findElement(By.id(CREATE_BTN_ID));
+        }
+
+        public WebElement getContactGrid()
         {
             return browser.findElement(By.id(GRID_ID));
         }
 
-        public WebElement GetCreateButton(WebDriver browser)
+        public WebElement getCreateButton()
         {
             return browser.findElement(By.id(CREATE_BTN_ID));
         }
 
-        public WebElement GetGridRowById(WebDriver browser, String rowId)
+        public ContactPopUpPageObject getContactPopUp()
+        {
+            CreateButton.click();
+            ContactPopUpPageObject popup =  
+                new ContactPopUpPageObject(browser);
+            return popup;
+        }
+
+        public WebElement getGridRowById(String rowId)
         {
             return browser.findElement(By.id(rowId));
         }
@@ -39,21 +54,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
          *              48-Holmes
          * Ergo, we can use the CSS selector id$="Holmes" to match. Yay.
          */
-        public WebElement GetGridRowByContactName(WebDriver browser, String contactName)
+        public WebElement getGridRowByIdSubStringContactName( String contactName)
         {
             String selector = "tr[id$='" + contactName + "']";
             return browser.findElement(By.cssSelector(selector));
         }
 
-        public WebElement GetRowByRowTextContent (WebDriver browser, String contentText)
+        public WebElement getRowByRowTextContent (String contentText)
         {
-            String contentxpath = "//tbody/tr[contains(.," + contentText + ")]";
-            return browser.findElement(By.xpath(contentxpath));
+            String contentXpath = "//tbody/tr[contains(.," + contentText + ")]";
+            return browser.findElement(By.xpath(contentXpath));
         }
 
-        public Boolean WaitUntilGridIsPopulatedWithRows(WebDriver browser)
+        public Boolean waitUntilGridIsPopulatedWithRows(WebDriver browser)
         {
-            WebDriverWait wait = new WebDriverWait(browser, 30);
+            WebDriverWait wait = new WebDriverWait(browser, 30000);
             wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//tbody/tr")));
 
             return true;
